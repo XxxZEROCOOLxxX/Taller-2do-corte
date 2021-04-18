fb1bcadb6c5e46b31cb00bc88535b3f406a2b89f
Alexander González <79185106+XxxZEROCOOLxxX@users.noreply.github.com>
2021-04-18 10:54:18 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* Activador de JQuery */
+$(document).ready(function(){
+    $('.slick-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500, 
+        speed: 300,
+        arrows: true,
+        pauseOnHover: false,        
+    });
+});
+
